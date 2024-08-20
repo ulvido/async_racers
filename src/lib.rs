7dc07f8d3dc8533aefee63488382ff76f67223f4
Ulvi YELEN <ulvido@gmail.com>
2024-08-20 14:03:49 +0300

    denemeler

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,10 @@ use rand::Rng;
 /// // example race
 /// let race1 = Race {
 ///     name: "Monaco Grand Prix".into(),
-///     laps: 88,
+///     laps: 10,
 ///     racers: vec![],
 ///     winner: None,
 /// }
-///
 /// ```
 ///
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -34,21 +33,21 @@ impl<'a> Race<'a> {
         self.racers.push(racer);
     }
 
-    // pub async fn start(&mut self) -> Racer {
-    //     for lap in 0..self.laps {
-    //         self.racers.iter_mut().for_each(|racer| {
-    //             racer.get_mut().do_lap();
-    //         });
-    //     }
-    //     Racer::default()
-    // }
+    pub fn start_race_single_threaded(&mut self) -> Racer {
+        for lap in 0..self.laps {
+            for racer in self.racers.iter_mut() {
+                racer.clone().do_lap(lap);
+            }
+        }
+        Racer::default()
+    }
 }
 
 impl<'a> Default for Race<'a> {
     fn default() -> Self {
         Self {
             name: "".into(),
-            laps: 88,
+            laps: 10,
             racers: Vec::new(),
             winner: None,
         }
@@ -60,14 +59,14 @@ impl<'a> Default for Race<'a> {
 /// // example racer
 /// let racer1 = Racer {
 ///    name: "Schumaher".into(),
-///    current_lap: 0,
+///    completed_lap_count: 0,
 ///    best_lap_time: None,
 /// }
 /// ```
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Racer {
     pub name: String,
-    pub current_lap: u8,
+    pub completed_lap_count: u8,
     pub lap_times: Vec<u8>,
     pub best_lap_time: Option<u8>,
 }
@@ -76,7 +75,7 @@ impl Default for Racer {
     fn default() -> Self {
         Self {
             name: "Schumaher".into(),
-            current_lap: 0,
+            completed_lap_count: 0,
             lap_times: Vec::new(),
             best_lap_time: None,
         }
@@ -93,11 +92,14 @@ impl Racer {
             ..Default::default()
         }
     }
-    pub async fn do_lap(mut self) {
-        let laptime: u8 = rand::thread_rng().gen();
+    pub fn do_lap(mut self, lap: u8) {
+        // let laptime: u8 = rand::thread_rng().gen();
+        // tokio::time::sleep(tokio::time::Duration::from_millis(laptime.into())).await;
+        let laptime: u8 = rand::thread_rng().gen_range(99..=255);
+        std::thread::sleep(std::time::Duration::from_millis(laptime.into()));
+        println!("{} finished lap {} in {}ms.", self.name, lap, laptime);
         self.lap_times.push(laptime);
-        tokio::time::sleep(tokio::time::Duration::from_millis(laptime.into())).await;
-        self.current_lap += 1;
+        self.completed_lap_count += 1;
         if let Some(best_lap_time) = self.best_lap_time {
             if laptime < best_lap_time {
                 self.best_lap_time = Some(laptime)
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ fn main() {
     let mut race1 = Race::with_name("İstanbul Grandprix");
 
     let racer1 = Racer::with_name("Ulvi Yelen");
+    let racer2 = Racer::with_name("Schumaher");
+    let racer3 = Racer::with_name("Hakkinen");
 
     race1.add_racer(&racer1);
-    println!("{race1:#?}");
-    println!("{racer1:#?}");
+    race1.add_racer(&racer2);
+    race1.add_racer(&racer3);
+
+    // println!("{race1:#?}");
+    // println!("{racer1:#?}");
+    
+    race1.start_race_single_threaded();
 }
